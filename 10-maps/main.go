19f2531f820e1e0c03f50cab3d5ba23946a49594
Atharva Wadodkar <atharva.wadodkar2003@gmail.com>
2026-06-11 12:21:01 +0530

    Add maps example

---
+++ 10-maps/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"maps"
+)
+
+func main() {
+
+	m := make(map[string]int)
+
+	m["k1"] = 7
+	m["k2"] = 13
+
+	fmt.Println("map:", m)
+
+	v1 := m["k1"]
+	fmt.Println("v1:", v1)
+
+	// If the key doesn’t exist, the zero value of the value type is returned.
+	v3 := m["k3"]
+	fmt.Println("v3:", v3)
+
+	fmt.Println("len:", len(m))
+
+	delete(m, "k2")
+	fmt.Println("map:", m)
+
+	// To remove all key/value pairs from a map, use the clear builtin
+	clear(m)
+	fmt.Println("map:", m)
+
+	// Check if "k2" exists in the map
+	// If yes, prs: true, else prs: false
+	_, prs := m["k2"]
+	fmt.Println("prs:", prs)
+
+	n := map[string]int{"foo": 1, "bar": 2}
+	fmt.Println("map:", n)
+
+	n2 := map[string]int{"foo": 1, "bar": 2}
+	if maps.Equal(n, n2) {
+		fmt.Println("n == n2")
+	}
+}
